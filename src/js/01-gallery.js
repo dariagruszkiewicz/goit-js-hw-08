c8b4b1017714b466c53386186a6bb6c210ec5a87
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-04-02 11:25:34 +0200

    update task 1
    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galeryListEl = document.querySelector('.gallery');
+
+const imgPreview = galleryItems.map(item => item.preview);
+const imgOriginal = galleryItems.map(item => item.original);
+const imgDescription = galleryItems.map(item => item.description);
+
+galleryItems.forEach(function (item, i) {
+  const li = document.createElement('li');
+  galeryListEl.append(li);
+  const a = document.createElement('a');
+  a.classList.add('gallery__item');
+  a.setAttribute('href', imgOriginal[i]);
+  li.append(a);
+  const newImg = document.createElement('img');
+  a.append(newImg);
+  item = newImg;
+  newImg.classList.add('gallery__image');
+  newImg.setAttribute('src', imgPreview[i]);
+  newImg.setAttribute('alt', imgDescription[i]);
+});
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+gallery.on('show.simplelightbox');
+
+galeryListEl.addEventListener('click', e => {
+  e.preventDefault();
+});
