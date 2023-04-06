ef35de7cbe2884f5e1f3252bfb6b982055db2861
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-04-06 19:41:44 +0200

    update task 2
    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
-import player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function (time) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
+  console.log('czas odtwarzania:', time.seconds);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
