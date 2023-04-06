23e95fe794718baf20a42f35db7e11d91843f3e2
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-04-06 22:26:17 +0200

    update task 2
    add throttle.lodash method

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,16 @@
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function (time) {
+const throttled = throttle(time => {
   localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify(time.seconds)
   );
   console.log('czas odtwarzania:', time.seconds);
-});
+}, 1000);
+
+player.on('timeupdate', throttled);
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+{
+  /* <form class="feedback-form" autocomplete="off">
+      <label>
+        Email
+        <input type="email" name="email" autofocus />
+      </label>
+      <label>
+        Message
+        <textarea name="message" rows="8"></textarea>
+      </label>
+      <button type="submit">Submit</button>
+    </form> */
+}
 
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+const inputEl = document.querySelector("input[type='email']");
+console.log(inputEl.value);
+
+const addToLocalStorage = localStorage.setItem(
+  'feedback-form-state',
+  inputEl.value
+);
+console.log(addToLocalStorage);
+formEl.addEventListener('submit', () => {});
