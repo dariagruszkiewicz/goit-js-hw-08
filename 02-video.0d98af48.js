a809fe5d24bca50d32710b8cb928b6e5a8ba357a
dariagruszkiewicz <117391460+dariagruszkiewicz@users.noreply.github.com>
2023-04-06 20:26:43 +0000

    Deploying to gh-pages from @ dariagruszkiewicz/goit-js-hw-08@23e95fe794718baf20a42f35db7e11d91843f3e2 🚀

--- 02-video.0d98af48.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e.seconds)),console.log("czas odtwarzania:",e.seconds)})),t.getVideoTitle().then((function(e){console.log("title:",e)})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"))}();
-//# sourceMappingURL=02-video.0d98af48.js.map
