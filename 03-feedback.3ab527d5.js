9c6f7475cd73dbbe42189e6390a4f80af78a6eff
dariagruszkiewicz <117391460+dariagruszkiewicz@users.noreply.github.com>
2023-04-07 20:49:58 +0000

    Deploying to gh-pages from @ dariagruszkiewicz/goit-js-hw-08@1c82be25e607f420510aa6cabc171fbf245118fd 🚀

--- 03-feedback.3ab527d5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.log(e);const o=document.querySelector("input[type='email']");console.log(o.value);const t=localStorage.setItem("feedback-form-state",o.value);console.log(t),e.addEventListener("submit",(()=>{}));
-//# sourceMappingURL=03-feedback.3ab527d5.js.map
--- 03-feedback.7d6bfb11.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e);var o=document.querySelector("input[type='email']");console.log(o.value);var t=localStorage.setItem("feedback-form-state",o.value);console.log(t),e.addEventListener("submit",(function(){}))}();
-//# sourceMappingURL=03-feedback.7d6bfb11.js.map
